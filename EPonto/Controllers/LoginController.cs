a5fa641c07730b1d7aa6f6bdee3f6d56094ce8e6
Kadu Scorsoni <scorsonik@gmail.com>
2025-03-30 21:07:04 -0300

    Iniciada a estrutura do login

---
+++ EPonto/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Domain.Entities.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPonto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginService _loginService;
+        public LoginController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        [Route("RealizarLogin")]
+        public ActionResult<ResultadoLoginModel> RealizarLogin(LoginModel paramLogin)
+        {
+
+            return new ResultadoLoginModel();
+        }
+    }
+}
--- EPonto/Program.cs
+++ EPonto/Program.cs
@@ -1,3 +1,8 @@
+using Application.Interfaces;
+using Application.Services;
+using Data.Interfaces;
+using Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Login
+//builder.Services.AddScoped<ILoginService, LoginService>();
+//builder.Services.AddScoped<ILoginRepository, LoginRepository>();
+
+
 
 var app = builder.Build();
 
