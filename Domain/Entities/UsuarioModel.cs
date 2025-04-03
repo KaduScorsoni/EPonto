29e09fb970e06136fe037fe4a6a18db93fc6bb12
CauaBrito01 <cauafranca115@gmail.com>
2025-04-03 18:47:34 -0300

    Estruturação do requisito de GerenciarUsuarios

---
+++ Domain/Entities/UsuarioModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UsuarioModel
+    {
+        public int ID_USUARIO { get; set; }
+        public string NOME { get; set; }
+        public DateTime DATA_NASCIMENTO { get; set; }
+        public string SENHA { get; set; }
+        public int BATIDA_ATUAL { get; set; }
+        public string EMAIL { get; set; }
+        public int ID_CARGO { get; set; }
+        public int ID_JORNADA { get; set; }
+        public int TELEFONE { get; set; }
+    }
+}
