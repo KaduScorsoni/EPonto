a62b106683999e68ce46f798d5a4df1895a68b14
CauaBrito01 <cauafranca115@gmail.com>
2025-04-14 16:59:50 -0300

    Correção

--- EPonto/Program.cs
+++ EPonto/Program.cs
@@ -6,34 +6,37 @@ using Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+// Definir a porta (verificando o ambiente)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080"; // Porta padrão para Railway
 builder.WebHost.UseUrls($"http://*:{port}");
 
-// Add services to the container.
-
+// Adicionar serviços à aplicação
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Login
+// Configuração dos serviços
 builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<ILoginRepository, LoginRepository>();
-
-
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<DbSession>();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+// Habilitar o Swagger se estiver no ambiente de Desenvolvimento ou Produção
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = string.Empty; // Exibe o Swagger diretamente na raiz
+    });
+}
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
