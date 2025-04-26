ce54abd25b5779604a4217fbb84cd615950279a7
CauaBrito01 <cauafranca115@gmail.com>
2025-04-26 17:08:40 -0300

    CRUD registro ponto

---
+++ Application/Interfaces/IRegistroPontoService.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IRegistroPontoService
+    {
+        Task<RegistroPontoDTO> CriarRegistroPontoAsync(RegistroPontoModel ponto);
+        Task<RegistroPontoDTO> ObterRegistroPontoIdAsync(int id);
+        Task<RegistroPontoDTO> ListarTodosRegistrosPontoAsync();
+        Task<RegistroPontoDTO> AtualizarRegistroPontoAsync(RegistroPontoModel ponto);
+        Task<RegistroPontoDTO> ExcluirRegistroPontoAsync(int id);
+    }
+}
---
+++ Domain/Entities/RegistroPontoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class RegistroPontoModel
+    {
+        public int Id_Registro { get; set; }
+        public int Id_Usuario { get; set; }
+        public DateTime Hora_Registro { get; set; }
+        public DateTime Data_Registro { get; set; }
+        public int Id_Tipo_Registro_Ponto { get; set; }
+    }
+}
